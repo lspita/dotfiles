e02931eaabd92786754a832386d05aa3c7ab8994
Ludovico M. Spitaleri <ludovico.spitaleri@gmail.com>
2024-07-09 19:35:33 +0200

    zsh colors

--- eza/.config/zsh/scripts/eza.sh
+++ eza/.config/zsh/scripts/eza.sh
@@ -1,1 +1,1 @@
-alias ll="eza --color=always --long --git --no-filesize --icons=always --no-time --no-user --no-permissions"
+alias l="eza --color=always --long --git --no-filesize --icons=always --no-time --no-user --no-permissions"
