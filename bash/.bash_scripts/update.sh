f53c75a3f5a91cc2a202ed9bf153f115afb56c06
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2024-11-22 14:39:57 +0100

    no freeze

--- bash/.bash_scripts/update.sh
+++ bash/.bash_scripts/update.sh
@@ -8,17 +8,15 @@ full-upgrade() {
 }
 
 system-upgrade() {
-    message="Backup $(date +"%Y-%m-%d %H:%M:%S %Z")"
-
-    if [[ ${#} > 0 ]]; then
-        message="${1}"
-    fi
-    
     full-upgrade
-    full-freeze
+
+    local commit_message="Backup $(date +"%Y-%m-%d %H:%M:%S %Z")"
+    if [[ ${#} > 0 ]]; then
+        commit_message="${1}"
+    fi    
 
     git -C "${DOTFILES_PATH}" add "${DOTFILES_PATH}"
     git -C "${DOTFILES_PATH}" status
-    git -C "${DOTFILES_PATH}" commit -m "${message}"
+    git -C "${DOTFILES_PATH}" commit -m "${commit_message}"
     git -C  "${DOTFILES_PATH}" push
 }
