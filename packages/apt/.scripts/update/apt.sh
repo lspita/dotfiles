8c06740d1e3efbc8e59f9548cf931312b7ca86a5
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2025-03-09 11:22:23 +0100

    Backup 2025-03-09 11:22:23 CET

--- packages/apt/.scripts/update/apt.sh
+++ packages/apt/.scripts/update/apt.sh
@@ -4,14 +4,14 @@ __list-packages() {
 
 __upgrade-packages() {
     sudo apt update
-    sudo apt full-upgrade
-    sudo apt autoremove
+    sudo apt full-upgrade -y
+    sudo apt autoremove -y
 }
 
 __install-packages() {
-    xargs -I {} sudo apt install {}
+    xargs -I {} sudo apt install -y {}
 }
 
 __uninstall-packages() {
-    xargs -I {} sudo apt purge {}
+    xargs -I {} sudo apt purge -y {}
 }
