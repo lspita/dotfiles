aa371de2979cc46eff509fd0493ea6b4475042dc
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2025-04-18 10:14:37 +0200

    wsl check

--- packages/wsl/.oh-my-zsh/custom/wsl.zsh
+++ packages/wsl/.oh-my-zsh/custom/wsl.zsh
@@ -1,5 +1,7 @@
-export BROWSER="/mnt/c/Program\ Files/Mozilla\ Firefox/firefox.exe"
+if [ $(systemd-detect-virt) = "wsl" ]; then
+    export BROWSER="/mnt/c/Program\ Files/Mozilla\ Firefox/firefox.exe"
 
-clean-zone-identifier() {
-    find . -type f -name "*:Zone.Identifier" -exec rm "{}" \;
-}
+    clean-zone-identifier() {
+        find . -type f -name "*:Zone.Identifier" -exec rm "{}" \;
+    }
+fi
