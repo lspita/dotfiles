46ccf8f2d149ea278a1c22bdddfe68672b95c316
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2025-04-19 09:31:33 +0200

    flatpak wip

---
+++ packages/flatpak/.scripts/update/flatpak.sh
@@ -0,0 +1,19 @@
+__check-requirements() {
+    __command-exists flatpak
+}
+
+__dump-packages() {
+    flatpak list --columns=application | tail -n +1
+}
+
+__upgrade() {
+    flatpak update -y
+}
+
+__install() {
+    xargs -I {} flatpak install -y {}
+}
+
+__uninstall() {
+    xargs -I {} flatpak remove -y {}
+}
