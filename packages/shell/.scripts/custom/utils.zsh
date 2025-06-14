7880e2e824faa4516147e9d00354c86cebfdad4a
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2025-06-14 19:00:18 +0200

    new open

--- packages/shell/.scripts/custom/utils.zsh
+++ packages/shell/.scripts/custom/utils.zsh
@@ -1,2 +1,14 @@
-alias open="xdg-open"
+if __is-wsl; then
+    export BROWSER="/mnt/c/Program\ Files/Mozilla\ Firefox/firefox.exe"
+
+    clean-zone-identifier() {
+        find . -type f -name "*:Zone.Identifier" -exec rm "{}" \;
+    }
+
+    alias winget=winget.exe
+    alias open="explorer.exe"
+else
+    alias open="xdg-open"
+fi
+
 alias purge-all="find . -mindepth 1 -maxdepth 1 -exec rm -rf '{}' \;"
--- packages/wsl/.scripts/custom/wsl.zsh
+++
@@ -1,9 +0,0 @@
-if __is-wsl; then
-    export BROWSER="/mnt/c/Program\ Files/Mozilla\ Firefox/firefox.exe"
-
-    clean-zone-identifier() {
-        find . -type f -name "*:Zone.Identifier" -exec rm "{}" \;
-    }
-
-    alias winget=winget.exe
-fi
