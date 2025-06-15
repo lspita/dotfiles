8d85f55f2caaf1561e5e1e4ef4e80749990b5717
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2025-06-15 12:56:05 +0200

    new browser variable

--- packages/shell/.scripts/custom/utils.zsh
+++ packages/shell/.scripts/custom/utils.zsh
@@ -1,12 +1,11 @@
 if __is-wsl; then
-    export BROWSER="/mnt/c/Program\ Files/Mozilla\ Firefox/firefox.exe"
-
     clean-zone-identifier() {
         find . -type f -name "*:Zone.Identifier" -exec rm "{}" \;
     }
 
     alias winget=winget.exe
     alias open="explorer.exe"
+    export BROWSER="explorer.exe"
 else
     alias open="xdg-open"
 fi
